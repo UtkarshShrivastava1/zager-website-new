41e58b9625a1f8a3ebb3e8864a35e1536a480334
VVinaykumar003 <119816918+VVinaykumar003@users.noreply.github.com>
2025-12-19 11:20:00 +0530

    commitment
    New files to commit 19/12/25

--- frontend/src/data/images.js
+++
@@ -1,8 +0,0 @@
-const images = [
-  "https://images.pexels.com/photos/8566474/pexels-photo-8566474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/8566474/pexels-photo-8566474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/8566474/pexels-photo-8566474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/8566474/pexels-photo-8566474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-];
-
-export default images;

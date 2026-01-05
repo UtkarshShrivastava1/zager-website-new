b9a3391fb0b52aaf2f6e321e1984e4a607c0497f
RitikaV29 <149085840+RitikaV29@users.noreply.github.com>
2026-01-05 10:23:45 +0530

    add PWA support with manifest and icons
    - Updated package.json to include vite-plugin-pwa.
    - Added a new manifest.json file for PWA configuration.
    - Included zagerLogo-512x512.png as the app icon.
    - Modified vite.config.js to configure PWA settings.
    - Cleaned up main.jsx by adding spacing for readability.

--- frontend-zager/vite.config.js
+++ frontend-zager/vite.config.js
@@ -1,9 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+    
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "Zager",
+        short_name: "Zager",
+        background_color: "#051224",
+        theme_color: "#051224",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icons/zagerLogo-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
-  
